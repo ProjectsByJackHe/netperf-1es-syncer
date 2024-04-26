caf4b0026928ded8d5bd674018a87b5d16a6a893
Jack He <jackhe2345@gmail.com>
2024-04-26 09:15:30 -0700

    make simple KV store

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 import express from 'express';
 
 const app = express();
+const state = {};
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-const port = process.env.PORT || 8080;
+app.get('/setkeyvalue', (req, res) => {
+    const key = req.query.key;
+    const value = req.query.value;
+    if (!key || !value) {
+        return res.status(400).send('Bad Request. Missing either githubworkflowId, environmentId or ipAddress');
+    }
+    state[key] = value;
+    res.send('Data has been synced');
+});
 
+app.get('/getkeyvalue', (req, res) => {
+    const key = req.query.key;
+    if (!key) {
+        return res.status(400).send('Bad Request. Missing githubworkflowId');
+    }
+    if (!state[key]) {
+        return res.status(404).send('Data not found');
+    }
+    res.send(state[key]);
+});
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log('Server is running on port 8080');
 });
