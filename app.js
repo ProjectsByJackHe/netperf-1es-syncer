763e3fdb64e61ab7ecb893e81640fe8fe620f466
Jack He <jackhe2345@gmail.com>
2024-04-26 00:33:06 -0700

    8080

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port 8080');
 });
